6a16b2dd0cef4a43958e8368451b1b0249e7cc7a
LoviMadaan <119787828+LoviMadaan@users.noreply.github.com>
2023-02-20 18:36:12 +0530

    javascript function

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-const bookList= document.querySelector('.books');
-const title= document.getElementById('title');
-const author= document.getElementById('author');
-const addBook= document.querySelector('#add-books');
+const bookList = document.querySelector('.books');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const addBook = document.querySelector('#add-books');
 
-const Book= function theBook(title,author){
-    this.title= title;
-    this.author= author;
-}
+const Book = function theBook(title, author) {
+  this.title = title;
+  this.author = author;
+};
 
+let storedData = [];
+
+function addBookToList(newBook) {
+  const bookTemp = `
+    <div class="book">
+      <h2>Title: ${newBook.title}</h2>
+      <h2>Author: ${newBook.author}</h2>
+      <button class="delete" type="button">Remove</button>
+      <hr>
+    </div>
+  `;
+  document.querySelector('.books').innerHTML += bookTemp:
+  return
+  document.querySelector('.books')
